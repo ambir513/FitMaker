d37568ca1e058cee67f595843c7e61e7682a5710
Amar Biradar <176784906+ambir513@users.noreply.github.com>
2025-05-30 17:20:43 +0530

    Update HoverProfile.tsx

--- src/components/HoverProfile.tsx
+++ src/components/HoverProfile.tsx
@@ -1,8 +1,6 @@
-import { CalendarIcon } from "lucide-react";
+
 import Link from "next/link";
 import Image from "next/image";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   HoverCard,
